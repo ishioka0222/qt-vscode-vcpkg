9527285b4919edca7872ad0e62a1488559e7baf7
Hiroki Ishioka <ishioka0222@gmail.com>
2023-02-04 21:31:26 +0900

    Qtでhello, world!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
-#include <iostream>
+#include <QApplication>
 
-int main() { std::cout << "hello, world!" << std::endl; }
+#include "widget.h"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+  Widget w;
+  w.show();
+  return a.exec();
+}
